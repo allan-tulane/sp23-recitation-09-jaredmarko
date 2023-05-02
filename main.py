d3f057a2a20e67d3fcc0d7f01af10ed1e2eecf64
jaredmarko <jaredmarko1@comcast.net>
2023-05-02 03:36:25 +0000

    Updated

--- main.py
+++ main.py
@@ -12,8 +12,36 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
+    # Initialize S and U.
+    S = set([source])
+    U = set(graph.keys()) - S
+
+    # Initialize D.
+    D = {}
+    for v in graph:
+        D[v] = (float("inf"), float("inf"))
+    D[source] = (0, 0)
+
+    # While U is not empty:
+    while U:
+        # Let v be the vertex in U with the smallest value of D[v][0].
+        v = min(U, key=lambda x: D[x][0])
+
+        # Remove v from U.
+        U.remove(v)
+
+        # For each neighbor w of v:
+        for w in graph[v]:
+            # If D[w][0] is greater than D[v][0] + w:
+            if D[w][0] > D[v][0] + w:
+                # Update D[w][0] to D[v][0] + w.
+                D[w][0] = D[v][0] + w
+                # Update D[w][1] to D[v][1] + 1.
+                D[w][1] = D[v][1] + 1
+
+    # Return D.
+    return D
+
     
 def test_shortest_shortest_path():
 
@@ -40,8 +68,35 @@ def bfs_path(graph, source):
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+    # Initialize a queue and a set.
+    queue = [source]
+    visited = set([source])
+
+    # Initialize a dictionary to store the parent of each vertex.
+    parent = {}
+    for v in graph:
+        parent[v] = None
+
+    # While the queue is not empty:
+    while queue:
+        # Let v be the vertex at the front of the queue.
+        v = queue.pop(0)
+
+        # For each neighbor w of v:
+        for w in graph[v]:
+            # If w is not visited:
+            if w not in visited:
+                # Mark w as visited.
+                visited.add(w)
+
+                # Add w to the queue.
+                queue.append(w)
+
+                # Set the parent of w to v.
+                parent[w] = v
+
+    # Return the dictionary parent.
+    return parent
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -65,8 +120,25 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    # Initialize a list to store the path.
+    path = []
+
+    # Start at the destination node.
+    v = destination
+
+    # While v is not the source node:
+    while v != parents[v]:
+        # Add v's parent to the path.
+        path.append(v)
+
+        # Set v to v's parent.
+        v = parents[v]
+
+    # Reverse the path.
+    path.reverse()
+
+    # Return the path.
+    return path
 
 def test_get_path():
     graph = get_sample_graph()
